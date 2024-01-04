a68f68205dfa6a27574522647c05db30255432fc
Alireza <arhodjjati@gmail.com>
2024-01-04 17:32:31 +0100

    time 1:32

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-let saveEl = document.getElementById('save-el')
-let countEl = document.getElementById('count-el')
-let count = 0
+let num1 = 8
+let num2 = 2
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
 
-function increment() {
-    count += 1
-    countEl.textContent = count
+// Create four functions: add(), subtract(), divide(), multiply()
+let sumEl = document.getElementById('sum-el')
+function add() {
+    let result = num1 + num2    
+    sumEl.textContent = 'Result: ' + result
 }
+function subtract() {
+    let result = num1 - num2
+    sumEl.textContent = 'Result: ' + result
+}
+function divide() {
+    let result = num1 / num2
+    sumEl.textContent = 'Result: ' + result
+}
+function multiply() {
+    let result = num1 * num2
+    sumEl.textContent = 'Result: ' + result
+}
+// Call the correct function when the user clicks on one of the buttons
+// Perform the given calculation using num1 and num2
+// Render the result of the calculation in the paragraph with id="sum-el"
 
-function save() {
-    let countStr = count + ' - '
-    saveEl.textContent += countStr
-    count = 0
-    countEl.textContent = count
-}
+
+// E.g. if the user clicks on the "Plus" button, you should render
+// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+
+
