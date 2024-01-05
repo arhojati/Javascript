017a0c9fa10e41ce5a2dc754fb469a4c200c472d
Alireza <arhodjjati@gmail.com>
2024-01-05 20:38:56 +0100

    Final

--- 04 Chrome Extention/index.js
+++ 04 Chrome Extention/index.js
@@ -1,29 +1,45 @@
 let myLeads = []
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
+const saveTabBtn = document.getElementById('save-tab-btn')
+const deleteBtn = document.getElementById('delete-btn')
 const ulEl = document.getElementById('ul-el')
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'))
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'))
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
+
+saveTabBtn.addEventListener('click', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem('myLeads', JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
+
+deleteBtn.addEventListener('dblclick', function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
 
 inputBtn.addEventListener('click', function() {
     myLeads.push(inputEl.value)
     inputEl.value = ''
     localStorage.setItem('myLeads', JSON.stringify(myLeads))
-    renderLeads()
+    render(myLeads)
 })
 
-function renderLeads() {
+function render(leads) {
     let listItems = ''
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         // listItems +=  "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
         listItems +=  `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                     ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                     ${leads[i]}
                 </a>
             </li>
         `
