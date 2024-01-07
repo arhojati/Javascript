cc60c8eaed64f47735aa82bac95e39145234210b
Alireza <arhodjjati@gmail.com>
2024-01-07 12:24:08 +0100

    Final with practices

--- amazon-project/scripts/amazon.js
+++ amazon-project/scripts/amazon.js
@@ -24,7 +24,7 @@ products.forEach((product) => {
             </div>
 
             <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
                 <option selected value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -40,7 +40,7 @@ products.forEach((product) => {
 
             <div class="product-spacer"></div>
 
-            <div class="added-to-cart">
+            <div class="added-to-cart js-added-to-cart-${product.id}">
             <img src="images/icons/checkmark.png">
             Added
             </div>
@@ -54,10 +54,12 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML
 
+const addedMessageTimeouts = {}
+
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     button.addEventListener('click', () => {
         const productId = button.dataset.productId
-
+        const quantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value)
         let matchingItem
 
         cart.forEach((item) => {
@@ -67,11 +69,11 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
         })
 
         if (matchingItem) {
-            matchingItem.quantity += 1
+            matchingItem.quantity += quantity
         } else {
             cart.push({
                 productId,
-                quantity: 1
+                quantity
             })
         }
 
@@ -81,5 +83,19 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
         })
 
         document.querySelector('.js-cart-quantity').innerHTML = cartQuantity
+
+        const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`)
+        addedMessage.classList.add('added-to-cart-visible')
+
+        const previousTimeoutId = addedMessageTimeouts[productId]
+        if (previousTimeoutId) {
+            clearTimeout(previousTimeoutId)
+        }
+        
+        const timeoutId = setTimeout(() => {
+            addedMessage.classList.remove('added-to-cart-visible')
+        }, 2000)
+
+        addedMessageTimeouts[productId] = timeoutId
     })
 })
